7269f36098aa96b147ab83aaf0e9daebb54ed824
BingWeb <154599235+BingWeb@users.noreply.github.com>
2024-02-16 13:03:17 +0700

    Add files via upload

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a href='https://www.instagram.com/binggnf?igsh=MWFueXBjaTMwejhiNA=='>Instagram</a>
+        <a href=''>Twitter</a>
+        <a href=''>LinkedIn</a>
+    </div>
+  )
+}
